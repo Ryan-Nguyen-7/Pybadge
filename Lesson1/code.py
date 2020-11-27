cca8af8e00d425bec70669bc05d81f3e11d88733
Ryan-Nguyen-7 <74927146+Ryan-Nguyen-7@users.noreply.github.com>
2020-11-27 13:05:13 -0500

    Update code.py

--- Lesson1/code.py
+++ Lesson1/code.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python3
 
+# Created by Ryan Nguyen
+# Created on November 2020
+# This program is the "Space Aliens" program on the Pybadge
+
+import ugame
+import stage
+
+
+def game_scene():
+    # this function is the main game game_scene
+
+
+    print("\n\n\n") #  3 blank lines
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass #  just a palceholder for now
+
+
+if __name__ == "__main__":
+    game_scene()
